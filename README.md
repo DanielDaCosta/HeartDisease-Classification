f5bc2e9866473c78b35e616b18492f6d48286ab9
Daniel Da Costa <danieldacosta@Daniel-MacBook-Pro.local>
2019-06-21 11:36:06 -0300

    Adding readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# CyberLabs_Internship
+# CyberLabs_Internship_Prediction
+
+Prediction model built based on Mission_Prediction_Dataset.csv data that can predict the presence of a disease in the patient. 
+Model created as part of CYBERLABS Mission: Disease classification, Internship application.
+
+
--- teste1.py
+++ teste1.py
@@ -2,13 +2,32 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
+from scipy import stats
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from keras import Sequential
+from keras.layers import Dense
+from keras.callbacks import EarlyStopping
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
-
+# Reading csv file
 disease_data = pd.read_csv("/Users/danieldacosta/PycharmProjects/CyberLab/Mission_Prediction_Dataset.csv")
 
-# Analysing Data
-print(disease_data.shape)
+
+# Analyzing Data
+disease_data.shape
+disease_data.describe()
+features = ['column1', 'column4', 'column5', 'column8', 'column3', 'column7', 'column10', 'column11', 'column12', 'column13', 'column2', 'column6','column9','column14']
+disease_data = disease_data[features]  # Putting side by side features with the same input data type
 print(disease_data.describe())
+for i in range(disease_data.shape[1] - 1):
+    plt.figure(i+1)
+    column = [disease_data[features[i]].values]
+    sns.distplot(column)
+    plt.xlabel(features[i])
+
 
 # Analyzing Output
 data_size = disease_data.shape[0]
@@ -18,77 +37,59 @@ x = len(sick)/data_size
 y = len(not_sick)/data_size
 print('Sick :', x*100, '%')
 print('Not sick :', y*100, '%')
-
-plt.figure(1)
-labels = ['Sick','Not Sick']
-grafico = pd.value_counts(disease_data['column14'], sort=True)
-grafico.plot(kind = 'bar', rot=0)
+plt.figure(14)  # Plotting output feature for distribution analysis
+labels = ['Sick', 'Not Sick']
+graph = pd.value_counts(disease_data['column14'], sort=True)
+graph.plot(kind = 'bar', rot=0)
 plt.title("Transaction class distribution")
 plt.xticks(range(2), labels)
 plt.xlabel("Class")
 plt.ylabel("Frequency")
 
 
-### Preprocessing data
+# PREPROCESSING DATA
 
 # Checking for Missing Values
-print(disease_data.isnull().any().sum())
+disease_data.isnull().any().sum()
 
 #sns.pairplot(disease_data)
-plt.figure(2)
-sns.heatmap(disease_data.corr(), annot=True) #Correlation for the dataset
+plt.figure(15)
+sns.heatmap(disease_data.corr(), annot=True)  # Correlation Matrix of the Data
 
 
-# Checking and Removing Outliers
-from scipy import stats
+# Checking and Removing Outliers using Z-score function
 z = np.abs(stats.zscore(disease_data))
 threshold = 3
 disease_data = disease_data[(z < 3).all(axis=1)]
 
-features = disease_data.iloc[:, 0:13].columns
-#for i in range(disease_data.shape[1] - 1):
- #   plt.figure(i+3)
-  #  column = [disease_data[features[i]].values]
-   # sns.distplot(column)
-    #plt.xlabel('Column ' + str(i+1))
-#plt.show()
+
 #disease_data.hist()
 #from pandas.plotting import scatter_matrix
 #scatter_matrix(disease_data.iloc[1:, :]) #Correlacao entre os dados
 
 # Normalizing Data
 
-features = ['column1', 'column4', 'column5', 'column8', 'column3', 'column7', 'column10', 'column11', 'column12', 'column13', 'column2', 'column6','column9','column14']
-disease_data = disease_data[features]
-print(disease_data.describe())
-
-from sklearn.preprocessing import StandardScaler
-gaussian_features = disease_data.iloc[:, 0:4]
+gaussian_features = disease_data.iloc[:, 0:4]  # Features with a gaussian distribution were standardized
 sc = StandardScaler()
 gaussian_featuresX = sc.fit_transform(gaussian_features)
 
-from sklearn.preprocessing import MinMaxScaler
-categorical_features = disease_data.iloc[:, 4:10]
+
+categorical_features = disease_data.iloc[:, 4:10]  # Categorical features were normalized, except the binary ones
 sc = MinMaxScaler(feature_range=(0, 1))
 categorical_featuresX = sc.fit_transform(categorical_features)
 
 
-X = np.concatenate((gaussian_featuresX, categorical_featuresX, disease_data.iloc[:,10:13]), axis=1)
+X = np.concatenate((gaussian_featuresX, categorical_featuresX, disease_data.iloc[:, 10:13]), axis=1)  # Reassembling the data after nomalization
 Y = disease_data.iloc[:, 13]
 sc = StandardScaler()
 X = sc.fit_transform(X)
 
-# Splitting dataset
-from sklearn.model_selection import train_test_split
+# Splitting data for  Training and Test
+
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.30)
 
 
-### Creting Neural Network
-
-from keras import Sequential
-from keras.layers import Dense
-from keras.regularizers import l1
-from keras.layers import Dropout
+# Creating Neural Network
 
 classifier = Sequential()
 # First Hidden Layer
@@ -102,28 +103,29 @@ classifier.add(Dense(1, activation='sigmoid', kernel_initializer='random_normal'
 classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Early stopping
-from keras.callbacks import EarlyStopping
-es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=50)
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=0, patience=10)
 
 # Fitting the data to the training dataset
-history = classifier.fit(X_train, y_train, batch_size=30, epochs=200, validation_split=0.1, callbacks=[es])
+history = classifier.fit(X_train, y_train, batch_size=30, epochs=400, validation_split=0.1, callbacks=[es])
 eval_model = classifier.evaluate(X_train, y_train)
-print(eval_model)
+print('Train loss:', eval_model[0])
+print('Train Accuracy', eval_model[1])
 
-y_pred=classifier.predict(X_test)
-y_pred =(y_pred > 0.5)
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
 
-from sklearn.metrics import confusion_matrix
+# Confusion Matrix
 cm = confusion_matrix(y_test, y_pred)
-print(cm)
+print('Confusion Matrix: \n', cm)
 
-from sklearn.metrics import accuracy_score
-ac_str = accuracy_score(y_test, y_pred, normalize=True, sample_weight=None)
-print(ac_str)
+eval_test = classifier.evaluate(X_test,y_test)
+print('Test loss:', eval_test[0])
+print('Test Accuracy', eval_test[1])
 
 # Plot training history
-plt.figure(3)
+plt.figure(16)
 plt.plot(history.history['loss'], label='train')
 plt.plot(history.history['val_loss'], label='test')
 plt.legend()
-plt.show()
+#plt.show()
